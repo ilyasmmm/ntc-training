1fee8adb3a66e3caaf0bce4773c0593d5eb4bb5e
Muhammad Ilyas <mail.muhammadilyas@gmail.com>
2023-01-10 09:03:35 +0000

    lab 19

---
+++ labs/lab-19-task-3.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+import sys
+
+args = sys.argv
+
+print(f"HERE ARE MY ARGUMENTS: {args}")
+print(f"This is the first argument: {args[1]}")
+print(f"These are the useful args: {args[1:]}")
